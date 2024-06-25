dcce972f4667b086913f0c07a448c4d8a88fa120
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-06-24 17:05:14 -0700

    Set the validation for the signin

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,13 +1,49 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const isValidEmail = (email: string) => {
+    const emailRegex =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    if (!isValidEmail(email)) {
+      setError("Email is invalid");
+      return;
+    }
+
+    if (!password || password.length < 8) {
+      setError("Password is invalid");
+      return;
+    }
+
+    try {
+      router.push("/login");
+    } catch (error) {
+      setError("Error, try again");
+      console.log(error);
+    }
+  };
   return (
     <div className="justify-center mt-16">
       <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-lg">
         <h1 className="text-3xl font-semibold text-center text-purple-700">
           Register
         </h1>
-        <form className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label className="label">
               <span className="text-base label-text">Name</span>
@@ -15,6 +51,7 @@ const Register = () => {
             <input
               type="text"
               placeholder="Type here"
+              required
               className="input input-bordered w-full"
             />
           </div>
@@ -25,6 +62,7 @@ const Register = () => {
             <input
               type="text"
               placeholder="Type here"
+              required
               className="input input-bordered w-full"
             />
           </div>
@@ -33,14 +71,16 @@ const Register = () => {
               <span className="text-base label-text">Password</span>
             </label>
             <input
-              type="text"
+              type="password"
               placeholder="Type here"
+              required
               className="input input-bordered w-full"
             />
           </div>
           <button type="submit" className="btn btn-primary">
             Register
           </button>
+          <p className="text-red-600 text-[16px] ">{error && error}</p>
         </form>
         <div className="text-center text-gray-500 mt-4">- or -</div>
         <Link
